0e0b64332df15e3f883461ee88f93d73d7cdc1a0
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2026-09-19 00:40:47 +0600

    auth limit

---
+++ src/lib/express-authLimite.ts
@@ -0,0 +1,11 @@
+import rateLimit from "express-rate-limit";
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 20, // 20 requests
+  message: {
+    success: false,
+    message: "Too many requests. Please try again later.",
+    errors: [],
+  },
+});
--- src/module/auth/auth.routes.ts
+++ src/module/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authController from "./auth.controller";
 import { validationReq } from "../../midileware/validationReq";
 import { authValidation } from "./auth.validation";
+import { authLimiter } from '../../lib/express-authLimite';
 
 
 
@@ -12,8 +13,8 @@ router.post(
 	validationReq.validate(authValidation.userRegisterValidationSchema),
 	authController.createStudent,
 );
-router.post("/verified-email", authController.verifayAccount);
-router.post("/login", authController.login);
+router.post("/verified-email",authLimiter, authController.verifayAccount);
+router.post("/login",authLimiter, authController.login);
 router.post("/google-login", authController.googleLogin);
 
 export const authRouter = router;
